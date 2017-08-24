997b651fbcfc461681f398de2da5fea69fc7a526
zondahuman <zondahuman@gmail.com>
2017-08-24 19:05:08 +0800

    add

--- gpc-service/src/test/java/com/rpc/mail/client/test/GrpcClientTest.java
+++ gpc-service/src/test/java/com/rpc/mail/client/test/GrpcClientTest.java
@@ -1,9 +1,14 @@
 package com.rpc.mail.client.test;
 
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by abin on 2017/8/2 18:34.
@@ -15,10 +20,14 @@ import java.util.Date;
 public class GrpcClientTest {
     public static void main(String[] args) throws Exception {
 
-        String result1 = getMonth(0);
-        System.out.println("result1="+result1);
-        String result2 = getMonth("2016-11-02 21:10:31");
-        System.out.println("result2="+result2);
+//        String result1 = getMonth(0);
+//        System.out.println("result1="+result1);
+//        String result2 = getMonth("2016-11-02 21:10:31");
+//        System.out.println("result2="+result2);
+//        String result3 = notEquals("abin");
+//        System.out.println("result3="+result3);
+        switchCase1("LOAN");
+
     }
 
     public static String getMonth(Integer which){
@@ -40,4 +49,28 @@ public class GrpcClientTest {
         return month;
     }
 
+    public static String notEquals(String which) throws ParseException {
+        if(!StringUtils.equals(which, "lee") && !StringUtils.equals(which, "abin")){
+            return "enter";
+        }
+        return "quit";
+    }
+
+
+    public static void switchCase1(String param) {
+        switch (param) {
+            case "LOAN":
+                String result = param;
+                System.out.println("result=" + result);
+                break;
+            case "YM":
+                break;
+            case "TL":
+                break;
+            case "FU":
+                break;
+            default:
+                break;
+        }
+    }
 }
